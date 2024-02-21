ae216ee029cc37e2127bcf204002b1d0ccbb2920
joel alvarez <joel.alvarez.rodriguez@iessanmamede.com>
2024-02-21 15:25:32 +0100

    Commit inicial

---
+++ src/main/java/ad/apiud6alumno/ApiUd6AlumnoApplication.java
@@ -0,0 +1,13 @@
+package ad.apiud6alumno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUd6AlumnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUd6AlumnoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ad/apiud6alumno/ApiUd6AlumnoApplicationTests.java
@@ -0,0 +1,13 @@
+package ad.apiud6alumno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUd6AlumnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
